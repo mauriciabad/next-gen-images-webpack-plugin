bd0cae822c09063089733319c7ca4f818ce42b2a
Maurici Abad <maurici.abad@glovoapp.com>
2021-09-30 12:04:21 +0200

    Initial implementation

--- TODO.md
+++ TODO.md
@@ -1,2 +1,3 @@
 # TODO
 - a plugin that puts all the `<img>` tags inside a `<picture>` tag.
+- ignore img inside picture elements
--- example-project/plugin.js
+++ example-project/plugin.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { JSDOM } = require('jsdom')
+
 class MyPlugin {
   static defaultOptions = {
     outputFile: 'assets.md',
@@ -14,33 +17,66 @@ class MyPlugin {
   apply(compiler) {
     const pluginName = MyPlugin.name
 
-    const { RawSource } = compiler.webpack.sources
+    compiler.hooks.compilation.tap(pluginName, (compilation) => {
+      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+        'beforeEmit',
+        (data, callback) => {
+          data.html = this.replaceImgToPicture(data.html)
 
-    compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
-      compilation.hooks.processAssets.tap(
-        {
-          name: pluginName,
-          stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONS,
-          additionalAssets: true,
-        },
-        (assets) => {
-          Object.entries(assets)
-            .filter(([pathname]) => /.html$/i.test(pathname))
-            .forEach(([pathname, source]) => {
-              // Only html files
+          // compilation.emitAsset(
+          //   this.options.outputFile,
+          //   new RawSource('this is a test')
+          // )
 
-              console.log(source)
-
-              // TODO: modify the source so it has the picture elem
-            })
-
-          compilation.emitAsset(
-            this.options.outputFile,
-            new RawSource('this is a test')
-          )
+          callback(null, data)
         }
       )
     })
+
+    const { RawSource } = compiler.webpack.sources
+  }
+
+  /**
+   * @param {string} html
+   * @returns {string}
+   */
+  replaceImgToPicture(html) {
+    const dom = new JSDOM(html)
+    const document = dom.window.document
+
+    const images = document.querySelectorAll('img')
+    function __setAttribute(source, img, attr, newAttr) {
+      let value = img.getAttribute(attr)
+      if (value) {
+        source.setAttribute(
+          newAttr || attr,
+          value.replace(/\.(jpe?g|png|gif)/gi, '.webp')
+        )
+      }
+    }
+
+    for (let i = 0; i < images.length; i++) {
+      const img = images[i]
+      if (img.parentElement && img.parentElement.tagName === 'PICTURE') {
+        continue
+      }
+      const picture = document.createElement('picture')
+      const source = document.createElement('source')
+      source.setAttribute('type', 'image/webp')
+      __setAttribute(source, img, 'sizes')
+      __setAttribute(source, img, 'srcset')
+      __setAttribute(source, img, 'media')
+
+      if (!source.hasAttribute('srcset')) {
+        __setAttribute(source, img, 'src', 'srcset')
+      }
+
+      img.parentElement.insertBefore(picture, img)
+      picture.appendChild(source)
+      picture.appendChild(img)
+    }
+
+    return document.documentElement.outerHTML
   }
 }
 
