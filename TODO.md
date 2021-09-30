9c0a759aba30891f5a5531204ab60ae69e4e35d4
Maurici Abad <maurici.abad@glovoapp.com>
2021-09-30 14:57:17 +0200

    Create new asset

--- TODO.md
+++ TODO.md
@@ -1,3 +1,8 @@
 # TODO
 - a plugin that puts all the `<img>` tags inside a `<picture>` tag.
 - ignore img inside picture elements
+- ro Reporting Progress https://webpack.js.org/api/plugins/#reporting-progress
+- do tests 
+- publish npm package
+- use browserlist to know if pollifill is needed
+- typescript
--- example-project/plugin.js
+++ example-project/plugin.js
@@ -21,52 +21,82 @@ class MyPlugin {
       HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
         'beforeEmit',
         (data, callback) => {
-          data.html = this.replaceImgToPicture(data.html)
-
-          // compilation.emitAsset(
-          //   this.options.outputFile,
-          //   new RawSource('this is a test')
-          // )
+          data.html = this.replaceImgToPicture(compiler, compilation, data.html)
 
           callback(null, data)
         }
       )
     })
-
-    const { RawSource } = compiler.webpack.sources
   }
 
-  customSetAttribute(source, img, attr, newAttr) {
-    let value = img.getAttribute(attr)
-    if (value) {
-      source.setAttribute(
-        newAttr || attr,
-        value.replace(/\.(jpe?g|png|gif)/gi, '.webp')
-      )
-    }
+  // customSetAttribute(source, img, attr, newAttr) {
+  //   let value = img.getAttribute(attr)
+  //   if (value) {
+  //     source.setAttribute(
+  //       newAttr || attr,
+  //       value.replace(/\.(jpe?g|png|gif)/gi, '.webp')
+  //     )
+  //   }
+  // }
+
+  /**
+   * @param {import('webpack').Compiler} compiler
+   * @param {import('webpack').Compilation} compilation
+   * @param {HTMLImageElement} img
+   * @returns {string}
+   */
+  createNewImageAsset(compiler, compilation, img) {
+    const { RawSource } = compiler.webpack.sources
+
+    const imgPathname = img.getAttribute('src').replace(/^([^\/]*\/)+/g, '')
+    // const newImgPathname = imgPathname.replace(/\.(jpe?g|png|gif)/gi, '.webp')
+    const newImgPathname = 'new-img-' + imgPathname
+
+    const originalImgAsset = compilation.getAsset(imgPathname)
+    console.log(originalImgAsset)
+
+    // TODO: here use the squash lib to generate the webp
+    // TODO: once the webp is working, do the jxl format
+    const newContent = originalImgAsset.source.source()
+
+    compilation.emitAsset(newImgPathname, new RawSource(newContent))
+
+    return newImgPathname
   }
 
   /**
+   * @param {import('webpack').Compiler} compiler
+   * @param {import('webpack').Compilation} compilation
    * @param {string} html
    * @returns {string}
    */
-  replaceImgToPicture(html) {
+  replaceImgToPicture(compiler, compilation, html) {
     const dom = new JSDOM(html)
     const document = dom.window.document
 
+    /** @type {HTMLImageElement[]} */
     const images = document.querySelectorAll(':not(picture) > img')
 
     for (const img of images) {
+      const newImgPathname = this.createNewImageAsset(
+        compiler,
+        compilation,
+        img
+      )
+
       const picture = document.createElement('picture')
+
       const source = document.createElement('source')
       source.setAttribute('type', 'image/webp')
-      this.customSetAttribute(source, img, 'sizes')
-      this.customSetAttribute(source, img, 'srcset')
-      this.customSetAttribute(source, img, 'media')
+      source.setAttribute('src', newImgPathname)
 
-      if (!source.hasAttribute('srcset')) {
-        this.customSetAttribute(source, img, 'src', 'srcset')
-      }
+      // this.customSetAttribute(source, img, 'sizes')
+      // this.customSetAttribute(source, img, 'srcset')
+      // this.customSetAttribute(source, img, 'media')
+
+      // if (!source.hasAttribute('srcset')) {
+      //   this.customSetAttribute(source, img, 'src', 'srcset')
+      // }
 
       img.parentElement.insertBefore(picture, img)
       picture.appendChild(source)
