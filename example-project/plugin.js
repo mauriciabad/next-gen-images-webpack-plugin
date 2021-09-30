85492148752a8526b8d19e62a4d73b7d2c1244f0
Maurici Abad <maurici.abad@glovoapp.com>
2021-09-30 17:55:48 +0200

    Move functions outside the class

--- example-project/plugin.js
+++ example-project/plugin.js
@@ -22,7 +22,7 @@ class MyPlugin {
       HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapPromise(
         'beforeEmit',
         async (data) => {
-          data.html = await this.replaceImgToPicture(
+          data.html = await replaceImgToPicture(
             compiler,
             compilation,
             data.html
@@ -33,116 +33,121 @@ class MyPlugin {
       )
     })
   }
+}
 
-  // customSetAttribute(source, img, attr, newAttr) {
-  //   let value = img.getAttribute(attr)
-  //   if (value) {
-  //     source.setAttribute(
-  //       newAttr || attr,
-  //       value.replace(/\.(jpe?g|png|gif)/gi, '.webp')
-  //     )
-  //   }
-  // }
+/**
+ * @param {import('webpack').Compiler} compiler
+ * @param {import('webpack').Compilation} compilation
+ * @param {ImagePool} imagePool
+ * @param {HTMLImageElement} img
+ * @returns {Promise<{webp: string, jxl:string}>}
+ */
+async function createNewImageAssets(
+  compiler,
+  compilation,
+  imagePool,
+  imageElement
+) {
+  const { RawSource } = compiler.webpack.sources
 
-  /**
-   * @param {import('webpack').Compiler} compiler
-   * @param {import('webpack').Compilation} compilation
-   * @param {ImagePool} imagePool
-   * @param {HTMLImageElement} img
-   * @returns {Promise<{webp: string, jxl:string}>}
-   */
-  async createNewImageAssets(compiler, compilation, imagePool, imageElement) {
-    const { RawSource } = compiler.webpack.sources
-
-    const imageName = imageElement
-      .getAttribute('src')
-      .replace(/^([^\/]*\/)+/g, '')
-    const imageNameWithoutExtension = imageName.replace(/\.[^\.]+$/i, '')
-    const newImagesNames = {
-      webp: imageNameWithoutExtension + '.webp',
-      jxl: imageNameWithoutExtension + '.jxl',
-    }
-
-    const image = compilation.getAsset(imageName).source.buffer()
-    const newImages = await this.encodeImageToAllFormats(imagePool, image)
-
-    for (const format in newImages) {
-      compilation.emitAsset(
-        newImagesNames[format],
-        new RawSource(newImages[format])
-      )
-    }
-
-    return newImagesNames
+  const imageName = imageElement
+    .getAttribute('src')
+    .replace(/^([^\/]*\/)+/g, '')
+  const imageNameWithoutExtension = imageName.replace(/\.[^\.]+$/i, '')
+  const newImagesNames = {
+    webp: imageNameWithoutExtension + '.webp',
+    jxl: imageNameWithoutExtension + '.jxl',
   }
 
-  /**
-   * @param {ImagePool} imagePool
-   * @param {Buffer} originalImage
-   * @returns {Promise<{webp: Buffer, jxl:Buffer}>}
-   */
-  async encodeImageToAllFormats(imagePool, originalImage) {
-    const image = imagePool.ingestImage(originalImage)
+  const image = compilation.getAsset(imageName).source.buffer()
+  const newImages = await encodeImageToAllFormats(imagePool, image)
 
-    await image.encode({
-      webp: 'auto',
-      jxl: 'auto',
-    })
-
-    return {
-      webp: Buffer.from((await image.encodedWith.webp).binary),
-      jxl: Buffer.from((await image.encodedWith.jxl).binary),
-    }
+  for (const format in newImages) {
+    compilation.emitAsset(
+      newImagesNames[format],
+      new RawSource(newImages[format])
+    )
   }
 
-  /**
-   * @param {import('webpack').Compiler} compiler
-   * @param {import('webpack').Compilation} compilation
-   * @param {string} html
-   * @returns {Promise<string>}
-   */
-  async replaceImgToPicture(compiler, compilation, html) {
-    const dom = new JSDOM(html)
-    const document = dom.window.document
+  return newImagesNames
+}
 
-    /** @type {HTMLImageElement[]} */
-    const imageElements = document.querySelectorAll(':not(picture) > img')
-    const imagePool = new ImagePool()
+/**
+ * @param {ImagePool} imagePool
+ * @param {Buffer} originalImage
+ * @returns {Promise<{webp: Buffer, jxl:Buffer}>}
+ */
+async function encodeImageToAllFormats(imagePool, originalImage) {
+  const image = imagePool.ingestImage(originalImage)
 
-    for (const imageElement of imageElements) {
-      const newImgNames = await this.createNewImageAssets(
-        compiler,
-        compilation,
-        imagePool,
-        imageElement
-      )
+  await image.encode({
+    webp: 'auto',
+    jxl: 'auto',
+  })
 
-      const picture = document.createElement('picture')
-      imageElement.parentElement.insertBefore(picture, imageElement)
-
-      for (const format of ['jxl', 'webp']) {
-        const source = document.createElement('source')
-        source.setAttribute('type', `image/${format}`)
-        source.setAttribute('src', newImgNames[format])
-
-        // this.customSetAttribute(source, img, 'sizes')
-        // this.customSetAttribute(source, img, 'srcset')
-        // this.customSetAttribute(source, img, 'media')
-
-        // if (!source.hasAttribute('srcset')) {
-        //   this.customSetAttribute(source, img, 'src', 'srcset')
-        // }
-
-        picture.appendChild(source)
-      }
-
-      picture.appendChild(imageElement)
-    }
-
-    await imagePool.close()
-
-    return document.documentElement.outerHTML
+  return {
+    webp: Buffer.from((await image.encodedWith.webp).binary),
+    jxl: Buffer.from((await image.encodedWith.jxl).binary),
   }
 }
+
+/**
+ * @param {import('webpack').Compiler} compiler
+ * @param {import('webpack').Compilation} compilation
+ * @param {string} html
+ * @returns {Promise<string>}
+ */
+async function replaceImgToPicture(compiler, compilation, html) {
+  const dom = new JSDOM(html)
+  const document = dom.window.document
+
+  /** @type {HTMLImageElement[]} */
+  const imageElements = document.querySelectorAll(':not(picture) > img')
+  const imagePool = new ImagePool()
+
+  for (const imageElement of imageElements) {
+    const newImgNames = await createNewImageAssets(
+      compiler,
+      compilation,
+      imagePool,
+      imageElement
+    )
+
+    const picture = document.createElement('picture')
+    imageElement.parentElement.insertBefore(picture, imageElement)
+
+    for (const format of ['jxl', 'webp']) {
+      const source = document.createElement('source')
+      source.setAttribute('type', `image/${format}`)
+      source.setAttribute('src', newImgNames[format])
+
+      // customSetAttribute(source, img, 'sizes')
+      // customSetAttribute(source, img, 'srcset')
+      // customSetAttribute(source, img, 'media')
+
+      // if (!source.hasAttribute('srcset')) {
+      //   customSetAttribute(source, img, 'src', 'srcset')
+      // }
+
+      picture.appendChild(source)
+    }
+
+    picture.appendChild(imageElement)
+  }
+
+  await imagePool.close()
+
+  return document.documentElement.outerHTML
+}
+
+// customSetAttribute(source, img, attr, newAttr) {
+//   let value = img.getAttribute(attr)
+//   if (value) {
+//     source.setAttribute(
+//       newAttr || attr,
+//       value.replace(/\.(jpe?g|png|gif)/gi, '.webp')
+//     )
+//   }
+// }
 
 module.exports = { MyPlugin }
